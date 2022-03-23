86b3fcc5dc12017b64f50d9c16ddcf1cf254f125
thanhtungpfiev <thanhtungpfiev@gmail.com>
2022-03-23 21:45:18 +0700

    NumberCardGame 1st Attempt

--- LGECodeJamCoding/2021OnlineRound1/NumberCardGame/main.cpp
+++ LGECodeJamCoding/2021OnlineRound1/NumberCardGame/main.cpp
@@ -13,6 +13,7 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -65,21 +66,68 @@ string mul(vector<int>& S, vector<int>& D, int B)
     return result;
 }
 
+void genBinaryArray(vector<vector<int>>& binaryArray, vector<int>& c, int step)
+{
+    if (step == c.size()) {
+        if (count(c.begin(), c.end(), 1) + count(c.begin(), c.end(), 0) == c.size()) {
+            binaryArray.push_back(c);
+        }
+        return;
+    }
+    for (int i = 0; i < 2; ++i) {
+        c[step] = i;
+        genBinaryArray(binaryArray, c, step + 1);
+    }
+}
+
 void inputByReadConsole()
 {
     int T = 0;
     cin >> T;
     string s = "";
+    while (T --> 0) {
+        cin >> s;
+        vector<int> arr;
+        vector<int> S;
+        vector<int> D;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '6') {
+                arr.push_back(9);
+            } else {
+                arr.push_back(convertCharToNumber(s[i]));
+            }
+        }
+        sort(arr.begin(), arr.end(), greater<int>());
+        vector<vector<int>> binaryArray;
+        vector<int> c(arr.size());
+        genBinaryArray(binaryArray, c, 0);
+        long long result = 0;
+        for (int i = 0; i < binaryArray.size(); ++i) {
+            vector<int> e = binaryArray[i];
+            int cnt1 = 0, cnt2 = 0;
+            long long firstNum = 0, secondNum = 0;
+            for (int j = e.size() - 1; j >= 0; --j) {
+                if (e[j] == false) {
+                    firstNum += arr[j] * pow(10, cnt1);
+                    cnt1++;
+                } else {
+                    secondNum += arr[j] * pow(10, cnt2);
+                    cnt2++;
+                }
+            }
+            result = max(result, firstNum * secondNum);
+        }
+        cout << result << endl;
+    }
 }
 
 void inputByReadFile()
 {
-    ifstream fileInput("/media/sf_D_DRIVE/CodingPractices/LGECodeJamCoding/LGECodeJamCoding/2021OnlineRound1/NumberCardGame/input.txt");
+    ifstream fileInput("F:/Research/CodingPractice/LGECodeJamCoding/LGECodeJamCoding/2021OnlineRound1/NumberCardGame/input.txt");
     string str;
     fileInput >> str;
     int T = 0;
     fileInput >> T;
-    cout << T << endl;
     string s = "";
     while (T --> 0) {
         fileInput >> s;
@@ -94,16 +142,32 @@ void inputByReadFile()
             }
         }
         sort(arr.begin(), arr.end(), greater<int>());
-        /// TODO:
-        /// Tu day arr, xay dung 2^n cau hinh, voi true cho S va false cho D. Sau do sap xep S va D va tinh tich cua 2 phan tu do. Chon ra best
-
-//        cout << result << endl;
+        vector<vector<int>> binaryArray;
+        vector<int> c(arr.size());
+        genBinaryArray(binaryArray, c, 0);
+        long long result = 0;
+        for (int i = 0; i < binaryArray.size(); ++i) {
+            vector<int> e = binaryArray[i];
+            int cnt1 = 0, cnt2 = 0;
+            long long firstNum = 0, secondNum = 0;
+            for (int j = e.size() - 1; j >= 0; --j) {
+                if (e[j] == false) {
+                    firstNum += arr[j] * pow(10, cnt1);
+                    cnt1++;
+                } else {
+                    secondNum += arr[j] * pow(10, cnt2);
+                    cnt2++;
+                }
+            }
+            result = max(result, firstNum * secondNum);
+        }
+        cout << result << endl;
     }
     fileInput.close();
 }
 
 int main()
 {
-    inputByReadFile();
+    inputByReadConsole();
     return 0;
 }
