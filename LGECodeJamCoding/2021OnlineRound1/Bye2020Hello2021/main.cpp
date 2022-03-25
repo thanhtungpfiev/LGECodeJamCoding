4f2cfd65ef7f7d4d0b93b5c1648c4542cb40aa59
tung.dao <tung.dao@lge.com>
2022-03-25 18:22:48 +0700

    Bye2020Hello2021 Finish

--- LGECodeJamCoding/2021OnlineRound1/Bye2020Hello2021/main.cpp
+++ LGECodeJamCoding/2021OnlineRound1/Bye2020Hello2021/main.cpp
@@ -12,6 +12,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
@@ -62,6 +63,69 @@ string findSum(string str1, string str2)
 
     return str;
 }
+
+void bruteforce(const vector<string>& arr, const int& N)
+{
+    int cnt = 0;
+    for (int i = 0; i < N - 1; ++i) {
+        for (int j = i + 1; j < N; ++j) {
+            string str1 = arr[i];
+            string str2 = arr[j];
+            if (str1.length() + str2.length() < 6 || (str1.length() == 3 && str2.length() == 3)) {
+                continue;
+            }
+            if (str1.length() < str2.length()) {
+                swap(str1, str2);
+            }
+            string temp1 = str1.size() > 4 ? str1.substr(str1.length() - 4) : str1;
+            string temp2 = str2.size() > 4 ? str2.substr(str2.length() - 4) : str2;
+
+            if (findSum(temp1 ,temp2) != "2021") {
+                continue;
+            }
+            if (findSum(str1, str2).substr(0, 4) == "2020") {
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << endl;
+}
+
+vector<long long> generatePossibleSum()
+{
+    vector<long long> possibleSum;
+    possibleSum.push_back(202021);
+    possibleSum.push_back(20202021);
+    possibleSum.push_back(202002021);
+    possibleSum.push_back(202012021);
+    possibleSum.push_back(202022021);
+    possibleSum.push_back(202032021);
+    possibleSum.push_back(202042021);
+    possibleSum.push_back(202052021);
+    possibleSum.push_back(202062021);
+    possibleSum.push_back(202072021);
+    possibleSum.push_back(202082021);
+    possibleSum.push_back(202092021);
+
+    return possibleSum;
+}
+
+void greedy(map<long long, long long>& arr)
+{
+    long long cnt = 0;
+    vector<long long> possibleSum = generatePossibleSum();
+    for (auto const& [k, v] : arr) {
+        for (int i = 0; i < possibleSum.size(); ++i) {
+            int val  = possibleSum[i] - k;
+            if (arr.find(val) != arr.end()) {
+                cnt += v * arr[val];
+            }
+        }
+
+    }
+    cout << cnt / 2 << endl;
+}
+
 void inputByReadConsole()
 {
     int T = 0;
@@ -69,31 +133,30 @@ void inputByReadConsole()
     while (T --> 0) {
         int N = 0;
         cin >> N;
-        vector<string> arr;
-        string temp;
+//        vector<string> arr;
+//        string temp;
+//        for (int i = 0; i < N; ++i) {
+//            cin >> temp;
+//            arr.push_back(temp);
+//        }
+//        bruteforce(arr, N);
+        map<long long, long long> arr;
+        long long temp = 0;
         for (int i = 0; i < N; ++i) {
             cin >> temp;
-            arr.push_back(temp);
+            arr[temp] += 1;
         }
-        int cnt = 0;
-        for (int i = 0; i < N - 1; ++i) {
-            for (int j = i + 1; j < N; ++j) {
-                string result = findSum(arr[i], arr[j]);
-                if (result.size() > 4) {
-                    string temp1 = result.substr(0, 4);
-                    string temp2 = result.substr(result.size() - 4);
-                    if (temp1 == "2020" && temp2 == "2021") {
-                        cnt++;
-                    }
-                }
-            }
-        }
-        cout << cnt << endl;
+        greedy(arr);
+
     }
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
     inputByReadConsole();
     return 0;
 }
